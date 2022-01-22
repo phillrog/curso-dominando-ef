5a8fdb504b98ed027f15ac421bc90f8182503b24
Phillipe Roger Souza <phillrog@hotmail.com>
2022-01-22 15:22:53 -0300

    Criando e deletando banco com Database.Ensure

--- Curso/Program.cs
+++ Curso/Program.cs
@@ -9,7 +9,13 @@ namespace DominandoEFCore
     {
         static void Main(string[] args)
         {
+            EnsureCreatedAndDeleted();
+        }
 
+        static void EnsureCreatedAndDeleted() {
+            using var db = new Curso.Data.ApplicationContext();
+            //db.Database.EnsureCreated();
+            db.Database.EnsureDeleted();
         }
     }
 }
