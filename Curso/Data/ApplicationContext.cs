b474c9ac5f4cfd79ddecc74efdfde448f8147d2f
phillrog <phillrog@hotmail.com>
2022-02-10 20:29:32 -0300

    Configurando o Timeout do comando global

--- Curso/Data/ApplicationContext.cs
+++ Curso/Data/ApplicationContext.cs
@@ -18,7 +18,9 @@ namespace Curso.Data
         {
             const string strConnection="Server=localhost,1435;Database=DevIO2; User=sa; Password=yourStrong#@Teste;Trusted_Connection=False;Persist Security Info=False; pooling=false;MultipleActiveResultSets=True";
             optionsBuilder
-                .UseSqlServer(strConnection, p => { p.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery); p.MaxBatchSize(100); })
+                .UseSqlServer(strConnection, p => p.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery)
+                .MaxBatchSize(100)
+                .CommandTimeout(5))
                 .EnableSensitiveDataLogging()
                 .UseLazyLoadingProxies()
                 .LogTo(Console.WriteLine, LogLevel.Information);
