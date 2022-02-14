8a7bcff3ab32d6374a03bb5ac9cef0ab25104305
phillrog <phillrog@hotmail.com>
2022-02-14 18:11:40 -0300

    Índices

--- Curso/Data/ApplicationContext.cs
+++ Curso/Data/ApplicationContext.cs
@@ -52,6 +52,14 @@ namespace Curso.Data
 
             modelBuilder.Entity<Departamento>().Property(p => p.Id)
             .HasDefaultValueSql("NEXT VALUE FOR sequencias.MinhaSequencia");
+
+            modelBuilder
+                .Entity<Departamento>()
+                .HasIndex(p => new { p.Descricao, p.Ativo})
+                .HasDatabaseName("idx_meu_indice_composto")
+                .HasFilter("DESCRICAO IS NOT NULL")
+                .HasFillFactor(80)
+                .IsUnique();
         }
 
         public override void Dispose()
