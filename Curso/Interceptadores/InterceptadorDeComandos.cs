b361f1eb26af13608ead09794cebbdd8a05fc313
phillrog <phillrog@hotmail.com>
2022-03-02 13:32:01 -0300

    Aplicando hint NOLOCK nas consultas

--- Curso/Interceptadores/InterceptadorDeComandos.cs
+++ Curso/Interceptadores/InterceptadorDeComandos.cs
@@ -8,13 +8,20 @@ namespace Curso.Interceptadores
 {
     public class InterceptadorDeComandos : DbCommandInterceptor
     {
+        private static readonly Regex _tableRegex =
+            new Regex(@"(?<tableAlias>FROM +(\[.*\]\.)?(\[.*\]) AS (\[.*\])(?! WITH \(NOLOCK\)))",
+                RegexOptions.Multiline | 
+                RegexOptions.IgnoreCase | 
+                RegexOptions.Compiled);
+
         public override InterceptionResult<DbDataReader> ReaderExecuting(
             DbCommand command, 
             CommandEventData eventData, 
             InterceptionResult<DbDataReader> result)
         {
-            System.Console.WriteLine("[Sync] Entrei dentro do metodo ReaderExecuting");
-            return base.ReaderExecuting(command, eventData, result);
+            UsarNoLock(command);
+
+            return result;
         }
 
         public override ValueTask<InterceptionResult<DbDataReader>> ReaderExecutingAsync(
@@ -23,8 +30,18 @@ namespace Curso.Interceptadores
             InterceptionResult<DbDataReader> result, 
             CancellationToken cancellationToken = default)
         {
-            System.Console.WriteLine("[Async] Entrei dentro do metodo ReaderExecutingAsync");
-            return base.ReaderExecutingAsync(command, eventData, result,cancellationToken);
+            UsarNoLock(command);
+
+            return new ValueTask<InterceptionResult<DbDataReader>>(result);
+        }
+
+        private static void UsarNoLock(DbCommand command)
+        {
+            if (!command.CommandText.Contains("WITH (NOLOCK)")
+                && command.CommandText.StartsWith("-- Use NOLOCK"))
+            {
+                command.CommandText = _tableRegex.Replace(command.CommandText, "${tableAlias} WITH (NOLOCK)");
+            }
         }
     }
 }
