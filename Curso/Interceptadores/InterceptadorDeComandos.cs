19305f584dbba4dbcc30b5b19f880c458f64e2c6
phillrog <phillrog@hotmail.com>
2022-03-02 13:28:34 -0300

    Sobrescrevendo métodos da classe base

--- Curso/Interceptadores/InterceptadorDeComandos.cs
+++ Curso/Interceptadores/InterceptadorDeComandos.cs
@@ -1,9 +1,30 @@
+using System.Data.Common;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore.Diagnostics;
 
 namespace Curso.Interceptadores
 {
     public class InterceptadorDeComandos : DbCommandInterceptor
     {
-       
+        public override InterceptionResult<DbDataReader> ReaderExecuting(
+            DbCommand command, 
+            CommandEventData eventData, 
+            InterceptionResult<DbDataReader> result)
+        {
+            System.Console.WriteLine("[Sync] Entrei dentro do metodo ReaderExecuting");
+            return base.ReaderExecuting(command, eventData, result);
+        }
+
+        public override ValueTask<InterceptionResult<DbDataReader>> ReaderExecutingAsync(
+            DbCommand command, 
+            CommandEventData eventData, 
+            InterceptionResult<DbDataReader> result, 
+            CancellationToken cancellationToken = default)
+        {
+            System.Console.WriteLine("[Async] Entrei dentro do metodo ReaderExecutingAsync");
+            return base.ReaderExecutingAsync(command, eventData, result,cancellationToken);
+        }
     }
 }
